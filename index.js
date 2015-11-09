6f2b7717f2b1f517b9e2091a674052d4adadde4e
alvaromb <amedina@apsl.net>
2015-11-09 11:49:27 +0100

    Added new files.

---
+++ index.js
@@ -0,0 +1,9 @@
+import KeyboardAwareMixin from './lib/KeyboardAwareMixin'
+import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
+import KeyboardAwareListView from './lib/KeyboardAwareListView'
+
+export default {
+  KeyboardAwareMixin,
+  KeyboardAwareScrollView,
+  KeyboardAwareListView
+}
