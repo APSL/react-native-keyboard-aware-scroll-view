ea0ccac24dee872e60b5f68f813004bb9987fb9b
alvaromb <amedina@apsl.net>
2017-09-29 10:14:25 +0200

    closes #163
    Exported KeyboardAwareFlatList.

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 import KeyboardAwareMixin from './lib/KeyboardAwareMixin'
 import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
 import KeyboardAwareListView from './lib/KeyboardAwareListView'
+import KeyboardAwareFlatList from './lib/KeyboardAwareFlatList'
 
 export {
   KeyboardAwareMixin,
-  KeyboardAwareScrollView,
-  KeyboardAwareListView
+  KeyboardAwareListView,
+  KeyboardAwareFlatList,
+  KeyboardAwareScrollView
 }
