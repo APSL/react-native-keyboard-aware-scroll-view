f69a4fac927191451a95beb64ca697fd6dce478a
alvaromb <amedina@apsl.net>
2017-09-29 10:13:12 +0200

    Started to support r16

--- index.js
+++ index.js
@@ -4,8 +4,4 @@ import KeyboardAwareMixin from './lib/KeyboardAwareMixin'
 import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
 import KeyboardAwareListView from './lib/KeyboardAwareListView'
 
-export {
-  KeyboardAwareMixin,
-  KeyboardAwareScrollView,
-  KeyboardAwareListView
-}
+export { KeyboardAwareMixin, KeyboardAwareScrollView, KeyboardAwareListView }
