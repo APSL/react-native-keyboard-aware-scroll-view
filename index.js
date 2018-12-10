a4318d7d7ad5d88fab5a4df44aee22f87fdf6ac8
Ethan Neff <ethan.neff@eneff.com>
2018-12-10 00:02:09 -0800

    fix: remove React Native 57+ ListView deprecation warnings (#319)

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 import listenToKeyboardEvents from './lib/KeyboardAwareHOC'
 import KeyboardAwareScrollView from './lib/KeyboardAwareScrollView'
-import KeyboardAwareListView from './lib/KeyboardAwareListView'
 import KeyboardAwareFlatList from './lib/KeyboardAwareFlatList'
 import KeyboardAwareSectionList from './lib/KeyboardAwareSectionList'
 
 export {
   listenToKeyboardEvents,
-  KeyboardAwareListView,
   KeyboardAwareFlatList,
   KeyboardAwareSectionList,
   KeyboardAwareScrollView
