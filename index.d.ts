bdae2384cbef8f548452e0f8b5abae91f47cb434
史彬洪 <1114564087@qq.com>
2019-08-21 10:40:39 +0800

    add method typescrip define

--- index.d.ts
+++ index.d.ts
@@ -142,7 +142,6 @@ interface KeyboardAwareProps {
    */
   onKeyboardDidChangeFrame?: (frames: Object) => void
 }
-
 export type ScrollIntoViewOptions = {
   getScrollPosition?: (
     parentLayout: ElementLayout,
@@ -152,13 +151,13 @@ export type ScrollIntoViewOptions = {
 }
 interface KeyboardAwareScrollViewProps
   extends KeyboardAwareProps,
-  ScrollViewProps { }
+    ScrollViewProps {}
 interface KeyboardAwareFlatListProps<ItemT>
   extends KeyboardAwareProps,
-  FlatListProps<ItemT> { }
+    FlatListProps<ItemT> {}
 interface KeyboardAwareSectionListProps<ItemT>
   extends KeyboardAwareProps,
-  SectionListProps<ItemT> { }
+    SectionListProps<ItemT> {}
 
 interface KeyboardAwareState {
   keyboardSpace: number
@@ -173,23 +172,23 @@ declare class ScrollableComponent<P, S> extends React.Component<P, S> {
     reactNode: Object,
     extraHeight?: number,
     keyboardOpeningTime?: number
-  ) => void
+  ) => void  
   scrollIntoView: (
     element: React.Element<*>,
     options: ScrollIntoViewOptions = {}
   ) => void
 }
 
-export class KeyboardAwareMixin { }
+export class KeyboardAwareMixin {}
 export class KeyboardAwareScrollView extends ScrollableComponent<
   KeyboardAwareScrollViewProps,
   KeyboardAwareState
-  > { }
+> {}
 export class KeyboardAwareFlatList extends ScrollableComponent<
   KeyboardAwareFlatListProps<any>,
   KeyboardAwareState
-  > { }
+> {}
 export class KeyboardAwareSectionList extends ScrollableComponent<
   KeyboardAwareSectionListProps<any>,
   KeyboardAwareState
-  > { }
+> {}
