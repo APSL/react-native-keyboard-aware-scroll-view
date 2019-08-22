356b4f9b71e2c42e1ff0a4dd0dedd333feeb6dac
史彬洪 <1114564087@qq.com>
2019-08-22 14:53:35 +0800

    add

--- index.d.ts
+++ index.d.ts
@@ -169,13 +169,13 @@ export type ScrollIntoViewOptions = {
 }
 interface KeyboardAwareScrollViewProps
   extends KeyboardAwareProps,
-  ScrollViewProps { }
+  ScrollViewProps {}
 interface KeyboardAwareFlatListProps<ItemT>
   extends KeyboardAwareProps,
-  FlatListProps<ItemT> { }
+  FlatListProps<ItemT> {}
 interface KeyboardAwareSectionListProps<ItemT>
   extends KeyboardAwareProps,
-  SectionListProps<ItemT> { }
+  SectionListProps<ItemT> {}
 
 interface KeyboardAwareState {
   keyboardSpace: number
@@ -192,21 +192,21 @@ declare class ScrollableComponent<P, S> extends React.Component<P, S> {
     keyboardOpeningTime?: number
   ) => void
   scrollIntoView: (
-    element: JSX.Element,
+    element: React.Element,
     options: ScrollIntoViewOptions = {}
   ) => void
 }
 
-export class KeyboardAwareMixin { }
+export class KeyboardAwareMixin {}
 export class KeyboardAwareScrollView extends ScrollableComponent<
   KeyboardAwareScrollViewProps,
   KeyboardAwareState
-  > { }
+  > {}
 export class KeyboardAwareFlatList extends ScrollableComponent<
   KeyboardAwareFlatListProps<any>,
   KeyboardAwareState
-  > { }
+  > {}
 export class KeyboardAwareSectionList extends ScrollableComponent<
   KeyboardAwareSectionListProps<any>,
   KeyboardAwareState
-  > { }
+  > {}
