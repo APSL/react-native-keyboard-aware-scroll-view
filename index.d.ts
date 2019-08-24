90cb67a71885e04696101b383dbdb083ad4d6063
史彬洪 <1114564087@qq.com>
2019-08-24 15:54:44 +0800

    add

--- index.d.ts
+++ index.d.ts
@@ -10,23 +10,6 @@ import {
   SectionListProps
 } from 'react-native'
 
-export type ScrollPosition = {
-  x: number,
-  y: number,
-  animated: boolean
-}
-
-export type ElementLayout = {
-  x: number,
-  y: number,
-  width: number,
-  height: number
-}
-
-export type ContentOffset = {
-  x: number,
-  y: number
-}
 
 interface KeyboardAwareProps {
   /**
@@ -160,13 +143,7 @@ interface KeyboardAwareProps {
    */
   onKeyboardDidChangeFrame?: (frames: Object) => void
 }
-export type ScrollIntoViewOptions = {
-  getScrollPosition?: (
-    parentLayout: ElementLayout,
-    childLayout: ElementLayout,
-    contentOffset: ContentOffset
-  ) => ScrollPosition
-}
+
 interface KeyboardAwareScrollViewProps
   extends KeyboardAwareProps,
   ScrollViewProps {}
